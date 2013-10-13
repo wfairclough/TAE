d55a6ecb2d10e3c25d0e9cda6fac8b87bc37eee1
Will Fairclough <wfairclough@gmail.com>
2013-10-13 08:49:22 -0400

    Added an OS Type checker to the start scripts for OS X and Linux.

--- start-client.sh
+++ start-client.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=./TAEvalCommons/
+echo "Starting $OSTYPE client."
 
-TAEval-Client/TAEval-Client
+if [[ "$OSTYPE" == "linux-gnu" ]]
+then
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/TAEvalCommons/
+	TAEval-Client/TAEval-Client
+elif [[ "$OSTYPE" == *darwin* ]]
+then
+	export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$PWD/TAEvalCommons/
+	TAEval-Client/TAEval-Client.app/Contents/MacOS/TAEval-Client
+fi
--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export DYLD_LIBRARY_PATH=./TAEvalCommons/
 export LD_LIBRARY_PATH=./TAEvalCommons/
 
 TAEval-Server/TAEval-Server

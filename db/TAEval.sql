91806c662f2806804eb2eeeb1e0293c4c302bb26
Will Fairclough <wfairclough@gmail.com>
2013-10-27 21:34:14 -0400

    Added SQLite schema for TAEval

---
+++ db/TAEval.sql
@@ -0,0 +1,100 @@
+--SQLite TAEval Database
+
+
+--CREATE DATABASE TABLES
+--=======================
+
+CREATE TABLE IF NOT EXISTS Administrator (
+  id INTEGER PRIMARY KEY NOT NULL,
+  FOREIGN KEY(id) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS Course (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name varchar(8) NOT NULL,
+  semester int NOT NULL,
+  year int NOT NULL,
+  instructorid INTEGER NOT NULL,
+  FOREIGN KEY(instructorId) REFERENCES Instructor(id)
+);
+
+-- Join Table for many-to-many relationship between TAs and Courses
+CREATE TABLE IF NOT EXISTS TA_Courses (
+  courseid INTEGER NOT NULL,
+  taid INTEGER NOT NULL,
+  PRIMARY KEY (courseId, taId),
+  FOREIGN KEY(courseId) REFERENCES Course(id),
+  FOREIGN KEY(taId) REFERENCES TA(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Evaluation (
+  id INTEGER PRIMARY KEY NOT NULL,
+  taskid INTEGER NOT NULL,
+  rating int NOT NULL,
+  comment varchar(255),
+  FOREIGN KEY(taskId) REFERENCES Task(id)
+);
+
+CREATE TABLE IF NOT EXISTS Instructor (
+  id INTEGER PRIMARY KEY NOT NULL,
+  FOREIGN KEY(id) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS Task (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name varchar(45) NOT NULL,
+  description varchar(45) NOT NULL,
+  taid INTEGER NOT NULL,
+  courseid INTEGER NOT NULL,
+  FOREIGN KEY(taId) REFERENCES TA(id),
+  FOREIGN KEY(courseId) REFERENCES Course(id)
+);
+
+CREATE TABLE IF NOT EXISTS TA (
+  id INTEGER PRIMARY KEY NOT NULL,
+  FOREIGN KEY(id) REFERENCES User(id)
+);
+
+-- Abstract User table for TA, Instructor, and Administrator
+CREATE TABLE IF NOT EXISTS User (
+  id INTEGER PRIMARY KEY NOT NULL,
+  firstName varchar(100) NOT NULL,
+  lastName varchar(100) NOT NULL,
+  username varchar(100) NOT NULL,
+  type int NOT NULL
+);
+
+
+
+--INSERT TEST DATA
+--=======================
+
+begin transaction;
+
+
+--Insert default admin user
+INSERT OR REPLACE INTO USER (firstName, lastName, username, type) VALUES ( "super", "admin", "admin", 1 );
+INSERT OR REPLACE INTO ADMINISTRATOR (id) SELECT id from USER where username="admin";
+
+
+INSERT OR REPLACE INTO USER (firstName, lastName, username, type) VALUES ( "Dr. Christine", "Laurendeau", "claurendeau", 1 );
+INSERT OR REPLACE INTO INSTRUCTOR (id) SELECT id from USER where username="claurendeau";
+
+
+INSERT OR REPLACE INTO COURSE (name, semester, year, instructorId) VALUES ( "COMP3004", 1, 2013, (SELECT id from USER where username='claurendeau') );
+
+
+end transaction;
+
+
+
+
+
+
+
+
+
+
+
+

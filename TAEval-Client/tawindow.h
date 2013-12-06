18797ce6d7f911d09de160935f1314ca55cb688b
SeanMalcolmson <seanmalcolmson@cmail.carleton.ca>
2013-12-06 13:23:19 -0500

    ui for ta view layout

--- TAEval-Client/tawindow.h
+++ TAEval-Client/tawindow.h
@@ -5,12 +5,13 @@
 #include <teachingassistant.h>
 #include <task.h>
 #include <course.h>
+#include "abstractsubscriber.h"
 
 namespace Ui {
 class TaWindow;
 }
 
-class TaWindow : public QMainWindow
+class TaWindow : public QMainWindow, public AbstractSubscriber
 {
     Q_OBJECT
     
@@ -18,6 +19,10 @@ public:
     explicit TaWindow(TeachingAssistant* user, QWidget *parent = 0);
     ~TaWindow();
     
+    void updateTaskListForTa(QList<Task*> list);
+    // ADD remaining calls to abstract subsciber here //
+
+
 private:
     Ui::TaWindow *ui;
     TeachingAssistant* teachingAssistant;
